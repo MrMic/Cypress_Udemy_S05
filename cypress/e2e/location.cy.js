88e5d8667487f4d56a7f1b1c9d2852585b8d9129
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-29 16:30:14 +0200

    feat: use of Fixtures

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -3,20 +3,18 @@
 describe('share location', () => {
   // * HACK: HOOK __________________________________________________________
   beforeEach(() => {
+    cy.fixture('user-location.json').as('userLocation');
     cy.visit('/').then(win => {
-      // ______________________________________________________________________
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition')
-        .as('getUserPosition')
-        .callsFake((cb) => {
-          setTimeout(() => {
-            cb({
-              coords: {
-                latitude: 37.5,
-                longitude: 48.01
-              }
-            });
-          }, 100);
-        })
+      cy.get('@userLocation').then(fakePosition => {
+        // ______________________________________________________________________
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition')
+          .as('getUserPosition')
+          .callsFake((cb) => {
+            setTimeout(() => {
+              cb(fakePosition);
+            }, 100);
+          })
+      });
       // ______________________________________________________________________
       cy.stub(win.navigator.clipboard, 'writeText')
         .as('saveToClipboard')
@@ -38,11 +36,14 @@ describe('share location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get('@saveToClipboard').should('have.been.called');
-    cy.get('@saveToClipboard').should(
-      'have.been.calledWithMatch',
-      new RegExp(`${37.5}.*${48.01}.*${encodeURI(
-        'John Doe'
-      )}`)
-    );
+    cy.get('@userLocation').then(fakePosition => {
+      const { latitude, longitude } = fakePosition.coords;
+      cy.get('@saveToClipboard').should(
+        'have.been.calledWithMatch',
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI(
+          'John Doe'
+        )}`)
+      );
+    });
   });
 });

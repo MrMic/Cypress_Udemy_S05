52ddc4f7ae8df1949769db13c071d17e4813b071
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-07 16:56:32 +0200

    feat: Manipulate the Clock

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,63 +1,63 @@
 /// <reference types="cypress" />
 
-describe( 'share location', () => {
+describe("share location", () => {
   // * HACK: HOOK __________________________________________________________
-  beforeEach( () => {
-    cy.fixture( 'user-location.json' ).as( 'userLocation' );
-    cy.visit( '/' ).then( win => {
-      cy.get( '@userLocation' ).then( fakePosition => {
+  beforeEach(() => {
+    cy.clock();
+    cy.fixture("user-location.json").as("userLocation");
+    cy.visit("/").then((win) => {
+      cy.get("@userLocation").then((fakePosition) => {
         // ______________________________________________________________________
-        cy.stub( win.navigator.geolocation, 'getCurrentPosition' )
-          .as( 'getUserPosition' )
-          .callsFake( ( cb ) => {
-            setTimeout( () => {
-              cb( fakePosition );
-            }, 100 );
-          } )
-      } );
+        cy.stub(win.navigator.geolocation, "getCurrentPosition")
+          .as("getUserPosition")
+          .callsFake((cb) => {
+            setTimeout(() => {
+              cb(fakePosition);
+            }, 100);
+          });
+      });
       // ______________________________________________________________________
-      cy.stub( win.navigator.clipboard, 'writeText' )
-        .as( 'saveToClipboard' )
+      cy.stub(win.navigator.clipboard, "writeText")
+        .as("saveToClipboard")
         .resolves();
-      cy.spy( win.localStorage, 'setItem' ).as( 'storeLocation' );
-      cy.spy( win.localStorage, 'getItem' ).as( 'getStoredLocation' );
-    } );
-  } )
+      cy.spy(win.localStorage, "setItem").as("storeLocation");
+      cy.spy(win.localStorage, "getItem").as("getStoredLocation");
+    });
+  });
   // ______________________________________________________________________
-  it( 'should fetch the user location', () => {
-    cy.get( '[data-cy="get-loc-btn"]' ).click();
-    cy.get( '@getUserPosition' ).should( 'be.called' );
+  it("should fetch the user location", () => {
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get("@getUserPosition").should("be.called");
     // Button is disabled after location is fetched
-    cy.get( '[data-cy="get-loc-btn"]' ).should( 'be.disabled' );
-    cy.get( '[data-cy="actions"]' ).should( 'contain', 'Location fetched' );
-  } );
+    cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
+    cy.get('[data-cy="actions"]').should("contain", "Location fetched");
+  });
 
   // ______________________________________________________________________
-  it( 'should share a location URL', () => {
-    cy.get( '[data-cy="name-input"]' ).type( 'John Doe' );
-    cy.get( '[data-cy="get-loc-btn"]' ).click();
-    cy.get( '[data-cy="share-loc-btn"]' ).click();
-    cy.get( '@saveToClipboard' ).should( 'have.been.called' );
-    cy.get( '@userLocation' ).then( fakePosition => {
+  it("should share a location URL", () => {
+    cy.get('[data-cy="name-input"]').type("John Doe");
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get("@saveToClipboard").should("have.been.called");
+    cy.get("@userLocation").then((fakePosition) => {
       const { latitude, longitude } = fakePosition.coords;
-      cy.get( '@saveToClipboard' ).should(
-        'have.been.calledWithMatch',
-        new RegExp( `${latitude}.*${longitude}.*${encodeURI(
-          'John Doe'
-        )}` )
+      cy.get("@saveToClipboard").should(
+        "have.been.calledWithMatch",
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI("John Doe")}`),
       );
-      cy.get( '@storeLocation' ).should(
-        'have.been.calledWithMatch',
+      cy.get("@storeLocation").should(
+        "have.been.calledWithMatch",
         /John Doe/,
-        new RegExp( `${latitude}.*${longitude}.*${encodeURI( 'John Doe' )}` )
+        new RegExp(`${latitude}.*${longitude}.*${encodeURI("John Doe")}`),
       );
-    } );
-    cy.get( '@storeLocation' ).should( 'have.been.called' );
-    cy.get( '[data-cy="share-loc-btn"]' ).click();
-    cy.get( '@getStoredLocation' ).should( 'have.been.called' );
+    });
+    cy.get("@storeLocation").should("have.been.called");
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get("@getStoredLocation").should("have.been.called");
 
-    cy.get( '[data-cy="info-message' ).should( 'be.visible' );
-    cy.get( '[data-cy="info-message' ).should( 'have.class', 'visible' );
-    cy.get( '[data-cy="info-message' ).should( 'not.be.visible' );
-  } );
-} );
+    cy.get('[data-cy="info-message').should("be.visible");
+    cy.get('[data-cy="info-message').should("have.class", "visible");
+    cy.tick(2000);
+    cy.get('[data-cy="info-message').should("not.be.visible");
+  });
+});
